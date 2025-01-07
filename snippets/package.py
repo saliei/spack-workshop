a6089b9d407723b4237b04d35687e11727e0aac0
Saeid Aliei <saeidaliei@gmail.com>
2025-01-07 11:20:26 +0000

    Add sample spack package

---
+++ snippets/package.py
@@ -0,0 +1,8 @@
+from spack.package import *
+class PySkaSdpFunc(PythonPackage):
+    """Python bindings for SDP Processing Function Library, A collection of high-performance 
+    data processing utility functions for the Square Kilometre Array."""
+
+    homepage = "https://gitlab.com/ska-telescope/sdp/ska-sdp-func"
+    url = "https://gitlab.com/ska-telescope/sdp/ska-sdp-func/-/archive/1.2.0/ska-sdp-func-1.2.0.tar.gz"
+    git = "https://gitlab.com/ska-telescope/sdp/ska-sdp-func"
